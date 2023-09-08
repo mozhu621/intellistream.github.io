a069dca8f3138d43afdd4e7d517c380cadadddc4
shuhao_zhang <shuhao_zhang@sutd.edu.sg>
2023-09-08 10:53:55 +0800

    update

--- js/custom.js
+++ js/custom.js
@@ -1,24 +1,26 @@
 //document.addEventListener("DOMContentLoaded", function() {
 
-  // Function to sort and categorize presentations
-  function sortAndCategorizePresentations(presentations) {
-    const past = [], future = [];
-    let next = null;
-    const currentDate = new Date();
-    presentations.sort((a, b) => new Date(a.date) - new Date(b.date));
-    presentations.forEach(presentation => {
-      const presentationDate = new Date(presentation.date);
-      if (presentationDate < currentDate) {
-        past.push(presentation);
-      } else if (presentationDate >= currentDate) {
-        future.push(presentation);
+    // Function to sort and categorize presentations
+    function sortAndCategorizePresentations(presentations) {
+      const past = [], future = [];
+      let next = null;
+      const currentDate = new Date();
+      currentDate.setHours(0, 0, 0, 0); // Set time to midnight for accurate comparison
+      presentations.sort((a, b) => new Date(a.date) - new Date(b.date));
+      presentations.forEach(presentation => {
+        const presentationDate = new Date(presentation.date);
+        presentationDate.setHours(0, 0, 0, 0); // Set time to midnight for accurate comparison
+        if (presentationDate < currentDate) {
+          past.push(presentation);
+        } else {
+          future.push(presentation);
+        }
+      });
+      if (future.length > 0) {
+        next = future.shift();
       }
-    });
-    if (future.length > 0) {
-      next = future.shift();
+      return { past, next, future };
     }
-    return { past, next, future };
-  }
 
     // Function to display presentations
     function displayPresentations(presentations, elementId) {
@@ -26,46 +28,56 @@
     element.innerHTML = ''; // Clear existing content    
     presentations.forEach(presentation => {
     const presentationDiv = `
-      <div>
-        <h3>${presentation.presenter} will present a ${presentation.paper}</h3>
-        <p>Categories: ${presentation.categories.join(', ')}</p>        
-        <p>Date: ${presentation.date}</p>
-        <p>Time: ${presentation.time}</p>
-        <p>Link: ${presentation.link}</p>
-        <p>Slides: <a href="${presentation.slides}" target="_blank">Download Slides</a></p>
-      </div>
-    `;
-    element.innerHTML += presentationDiv;
-  });
-}
+          <div>
+            <h3>${presentation.presenter} will present a ${presentation.paper}</h3>
+            <p>Categories: ${presentation.categories.join(', ')}</p>        
+            <p>Date: ${presentation.date}</p>
+            <p>Time: ${presentation.time}</p>
+            <p>Link: ${presentation.link}</p>
+            <p>Slides: <a href="${presentation.slides}" target="_blank">Download Slides</a></p>
+          </div>
+        `;
+        element.innerHTML += presentationDiv;
+        });
+    }
 
 
-  // Function to display the immediate next presentation
-  function displayNextPresentation(presentation, elementId) {
-    const element = document.getElementById(elementId);
-    const currentDate = new Date();
-    const nextWeekDate = new Date(currentDate);
-    nextWeekDate.setDate(currentDate.getDate() + 7); // 7 days from today
-  
-    if (presentation) {
-    const presentationDate = new Date(presentation.date);
+    // Function to display the immediate next presentation
+    function displayNextPresentation(presentation, elementId) {
+      const element = document.getElementById(elementId);
+      if (!element) {
+        console.error(`Element with id ${elementId} not found.`);
+        return;
+      }
+
+      const currentDate = new Date();
+      currentDate.setHours(0, 0, 0, 0); // Set time to midnight
+      const nextWeekDate = new Date(currentDate);
+      nextWeekDate.setDate(currentDate.getDate() + 7); // 7 days from today
+      nextWeekDate.setHours(0, 0, 0, 0); // Set time to midnight
+
+      if (presentation) {
+        const presentationDate = new Date(presentation.date);
+        presentationDate.setHours(0, 0, 0, 0); // Set time to midnight
         if (presentationDate <= nextWeekDate) {
+          const categories = presentation.categories ? presentation.categories.join(', ') : 'N/A';
           const presentationDiv = `
             <div>
               <h3>${presentation.presenter} will present "${presentation.paper}"</h3>
-                <p>Categories: ${presentation.categories.join(', ')}</p>        
-                <p>Date: ${presentation.date}</p>
-                <p>Time: ${presentation.time}</p>
-                <p>Link: ${presentation.link}</p>
-                <p>Slides: <a href="${presentation.slides}" target="_blank">Download Slides</a></p>
+              <p>Categories: ${categories}</p>        
+              <p>Date: ${presentation.date}</p>
+              <p>Time: ${presentation.time}</p>
+              <p>Link: ${presentation.link}</p>
+              <p>Slides: <a href="${presentation.slides}" target="_blank">Download Slides</a></p>
             </div>
           `;
           element.innerHTML = presentationDiv;
           return;
         }
+      }
+      element.innerHTML = "No presentation scheduled";
     }
-    element.innerHTML = "No presentation scheduled";
-  }
+
 
   // Sort and categorize presentations for each category
   const databases = sortAndCategorizePresentations(databases_presentations);
