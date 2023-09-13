b73e7e52f7737476e147d747b6d2e9d6211d4933
shuhao_zhang <shuhao_zhang@sutd.edu.sg>
2023-09-13 12:08:13 +0800

    update

--- _site/js/custom.js
+++ _site/js/custom.js
@@ -1,24 +1,26 @@
 //document.addEventListener("DOMContentLoaded", function() {
 
-  // Function to sort and categorize presentations
-  function sortAndCategorizePresentations(presentations) {
-    const past = [], future = [];
-    let next = null;
-    const currentDate = new Date();
-    presentations.sort((a, b) => new Date(a.date) - new Date(b.date));
-    presentations.forEach(presentation => {
-      const presentationDate = new Date(presentation.date);
-      if (presentationDate < currentDate) {
-        past.push(presentation);
-      } else if (presentationDate >= currentDate) {
-        future.push(presentation);
+    // Function to sort and categorize presentations
+    function sortAndCategorizePresentations(presentations) {
+      const past = [], future = [];
+      let next = null;
+      const currentDate = new Date();
+      currentDate.setHours(0, 0, 0, 0); // Set time to midnight for accurate comparison
+      presentations.sort((a, b) => new Date(a.date) - new Date(b.date));
+      presentations.forEach(presentation => {
+        const presentationDate = new Date(presentation.date);
+        presentationDate.setHours(0, 0, 0, 0); // Set time to midnight for accurate comparison
+        if (presentationDate < currentDate) {
+          past.push(presentation);
+        } else {
+          future.push(presentation);
+        }
+      });
+      if (future.length > 0) {
+        next = future.shift();
       }
-    });
-    if (future.length > 0) {
-      next = future.shift();
+      return { past, next, future };
     }
-    return { past, next, future };
-  }
 
     // Function to display presentations
     function displayPresentations(presentations, elementId) {
@@ -26,56 +28,73 @@
     element.innerHTML = ''; // Clear existing content    
     presentations.forEach(presentation => {
     const presentationDiv = `
-      <div>
-        <h3>${presentation.presenter} will present a ${presentation.paper}</h3>
-        <p>Categories: ${presentation.categories.join(', ')}</p>
-        <p>Date: ${presentation.date}</p>
-        <p>Time: ${presentation.time}</p>
-        <p>Link: ${presentation.link}</p>
-      </div>
-    `;
-    element.innerHTML += presentationDiv;
-  });
-}
+          <div>
+            <h3>${presentation.presenter} will present paper titled: ${presentation.paper}</h3>
+            <p>Categories: ${presentation.categories.join(', ')}</p>        
+            <p>Date: ${presentation.date}</p>
+            <p>Time: ${presentation.time}</p>
+            <p>Link: <a href="${presentation.link}" target="_blank">Join Zoom Meeting</a></p>
+            <p>Slides: <a href="${presentation.slides}" target="_blank">Download Slides</a></p>
+          </div>
+        `;
+        element.innerHTML += presentationDiv;
+        });
+    }
 
 
-  // Function to display the immediate next presentation
-  function displayNextPresentation(presentation, elementId) {
-    const element = document.getElementById(elementId);
-    const presentationDiv = `
-      <div>
-        <h3>${presentation.presenter} will present ${presentation.paper}</h3>
-        <p>Date: ${presentation.date}</p>
-        <p>Time: ${presentation.time}</p>
-        <p>Link: ${presentation.link}</p>
-      </div>
-    `;
-    element.innerHTML = presentationDiv;
-  }
+    // Function to display the immediate next presentation
+    function displayNextPresentation(presentation, elementId) {
+      const element = document.getElementById(elementId);
+      if (!element) {
+        console.error(`Element with id ${elementId} not found.`);
+        return;
+      }
+
+      const currentDate = new Date();
+      currentDate.setHours(0, 0, 0, 0); // Set time to midnight
+      const nextWeekDate = new Date(currentDate);
+      nextWeekDate.setDate(currentDate.getDate() + 7); // 7 days from today
+      nextWeekDate.setHours(0, 0, 0, 0); // Set time to midnight
+
+      if (presentation) {
+        const presentationDate = new Date(presentation.date);
+        presentationDate.setHours(0, 0, 0, 0); // Set time to midnight
+        if (presentationDate <= nextWeekDate) {
+          const categories = presentation.categories ? presentation.categories.join(', ') : 'N/A';
+          const presentationDiv = `
+          <div>
+            <h3>${presentation.presenter} will present paper titled: ${presentation.paper}</h3>
+            <p>Categories: ${presentation.categories.join(', ')}</p>        
+            <p>Date: ${presentation.date}</p>
+            <p>Time: ${presentation.time}</p>
+            <p>Link: <a href="${presentation.link}" target="_blank">Join Zoom Meeting</a></p>
+            <p>Slides: <a href="${presentation.slides}" target="_blank">Download Slides</a></p>
+          </div>
+        `;
+          element.innerHTML = presentationDiv;
+          return;
+        }
+      }
+      element.innerHTML = "No presentation scheduled";
+    }
+
 
   // Sort and categorize presentations for each category
-  const databases = sortAndCategorizePresentations(databases_presentations);
+  const system = sortAndCategorizePresentations(system_presentations);
   const machine_learning = sortAndCategorizePresentations(machine_learning_presentations);
-  const transactional_stream_processing = sortAndCategorizePresentations(transactional_stream_processing_presentations);
 
   // Display past, next, and future presentations for each category
-  displayPresentations(databases.past, 'databases_past');
-  if (databases.next) {
-    displayNextPresentation(databases.next, 'databases_next');
+  displayPresentations(system.past, 'system_past');
+  if (system.next) {
+    displayNextPresentation(system.next, 'system_next');
   }
-  displayPresentations(databases.future, 'databases_future');
+  displayPresentations(system.future, 'system_future');
 
   displayPresentations(machine_learning.past, 'machine_learning_past');
   if (machine_learning.next) {
     displayNextPresentation(machine_learning.next, 'machine_learning_next');
   }
   displayPresentations(machine_learning.future, 'machine_learning_future');
-
-  displayPresentations(transactional_stream_processing.past, 'transactional_stream_processing_past');
-  if (transactional_stream_processing.next) {
-    displayNextPresentation(transactional_stream_processing.next, 'transactional_stream_processing_next');
-  }
-  displayPresentations(transactional_stream_processing.future, 'transactional_stream_processing_future');
 
   // Example function to toggle visibility
   function toggleVisibility(section) {
@@ -87,10 +106,9 @@
     }
   }
 
-    const all_presentations = [...databases_presentations.map(p => ({ ...p, category: 'Databases' })), ...machine_learning_presentations.map(p => ({ ...p, category: 'Machine Learning' })), ...transactional_stream_processing_presentations.map(p => ({ ...p, category: 'Transactional Stream Processing' }))];
+    const all_presentations = [...system_presentations.map(p => ({ ...p, category: 'System' })), ...machine_learning_presentations.map(p => ({ ...p, category: 'Machine Learning' }))];
 
     const fuse = new Fuse(all_presentations, { keys: ['presenter', 'paper', 'date', 'time', 'categories'], threshold: 0.3 });
-
 
 function searchPresentations() {
   const query = document.getElementById('search-input').value;
